ccb426f513e1842ba49e83c6c3a2d1c16eb52778
BT_1N3_26@BKTECH.LOCAL <BT_1N3_26@BKTECH.LOCAL>
2018-10-10 14:48:01 -0400

    Initial commit

---
+++ src/BTsangLib.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class BTsangLib
+{
+    public static void main(String[] args)
+    {
+        System.out.print("Welcome! What is your name?");
+        Scanner name = new.Scanner(System.in);
+        System.out.print("Hey " + name + "! Do you want to guess a number or do you want an AI to guess your number? Press 1 for former and 2 for latter." );
+        
+        System.out.print("Guess");
+    }
+}
